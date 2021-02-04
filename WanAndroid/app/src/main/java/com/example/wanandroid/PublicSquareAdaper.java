b1596ee0d5c7048f8057d08ff73be0fe7828c5b8
shswz176 <yimifendou176@qq.com>
2021-02-04 17:57:53 +0800

    广场（TOTD分享人文章列表）

---
+++ WanAndroid/app/src/main/java/com/example/wanandroid/PublicSquareAdaper.java
@@ -0,0 +1,76 @@
+package com.example.wanandroid;
+
+import android.content.Intent;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import java.util.List;
+
+public class PublicSquareAdaper extends RecyclerView.Adapter<PublicSquareAdaper.ViewHolder>{
+    private List<UsefulData> mdata;
+
+    static class ViewHolder extends RecyclerView.ViewHolder {
+        TextView textView_title;
+        TextView textView_chapterName;
+        TextView textView_niceTime;
+        TextView textView_shareUser;
+
+        public ViewHolder(View view) {
+            super(view);
+            textView_chapterName = view.findViewById(R.id.s_article_chapterName);
+            textView_niceTime = view.findViewById(R.id.s_article_niceTime);
+            textView_title = view.findViewById(R.id.s_article_title);
+            textView_shareUser = view.findViewById(R.id.s_shareUser);
+
+        }
+    }
+
+    public PublicSquareAdaper(List<UsefulData> mdata) {
+        this.mdata = mdata;
+    }
+
+    @NonNull
+    @Override
+    public PublicSquareAdaper.ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.recycler_square_item, parent, false);
+        final PublicSquareAdaper.ViewHolder holder = new PublicSquareAdaper.ViewHolder(view);
+
+        holder.textView_title.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                int position = holder.getAdapterPosition();
+                UsefulData usefulData = mdata.get(position);
+                String link = usefulData.getLink();
+                Log.e("item_link",link);
+
+                Intent intent=new Intent(view.getContext(),web_activity.class);
+                intent.putExtra("links",link);
+                view.getContext().startActivity(intent);
+            }
+        });
+
+        return holder;
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull PublicSquareAdaper.ViewHolder holder, int position) {
+        UsefulData usefulData = mdata.get(position);
+        holder.textView_title.setText(usefulData.getTitle());
+        holder.textView_niceTime.setText(usefulData.getNiceDate());
+        holder.textView_chapterName.setText(usefulData.getSuperChapterName());
+        holder.textView_shareUser.setText(usefulData.getShareUser());
+
+    }
+
+    @Override
+    public int getItemCount() {
+        return mdata.size();
+    }
+
+}
